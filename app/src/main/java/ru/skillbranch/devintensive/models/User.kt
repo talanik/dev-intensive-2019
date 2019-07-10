586c979b4fb9d87a85f55170a97a7a5c5117cde2
talanik <51998681+talanik@users.noreply.github.com>
2019-07-10 22:35:55 +0500

    Update User.kt

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -4,30 +4,30 @@ import ru.skillbranch.devintensive.Utils.Utils
 import java.util.*
 
 data class User (
-    val id:String,
-    var firstName: String?,
-    var lastName: String?,
-    var avatar: String?,
-    var rating: Int = 0,
-    var respect: Int = 0,
-    val lastVisit: Date? = Date(),
-    val isOnline: Boolean = false
-) {
+    val id : String,
+    var firstName : String?,
+    var lastName : String?,
+    var avatar : String?,
+    var rating : Int = 0,
+    var respect : Int = 0,
+    val lastVisit : Date? = Date(),
+    val isOnline : Boolean = false
+){
+    constructor(id:String, firstName:String?, lastName:String?) : this(id, firstName, lastName, avatar = null)
 
-    constructor(id:String, firstName:String?, lastName:String?) : this(
-        id = id,
-        firstName = firstName,
-        lastName = lastName,
-        avatar = null
-    )
-
-    constructor(id:String) : this(id, firstName="John", lastName="Doe")
-
-    init {
-        println("it's Alive!!! \n" +
-                "${if(lastName==="Doe")  "His name is $firstName $lastName" else "And his name is $firstName $lastName!!!"}\n")
+    fun printMe():Unit{
+        println("""
+            id: $id
+            firstName: $firstName
+            lastName: $lastName
+            avatar: $avatar
+            rating: $rating
+            respect: $respect
+            lastVisit: $lastVisit
+            isOnline: $isOnline
+        """.trimIndent())
     }
-
+    
     companion object Factory{
         private var lastId: Int = -1
         fun makeUser(fullName: String?): User{
